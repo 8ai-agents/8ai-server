01d98fc3d49b686c7bf50bff5b9b0d971e6b74e9
James Bayly <james@bayly.xyz>
2024-04-11 22:31:40 +1200

    Update conversation status endpoint

--- src/DatabaseController.ts
+++ src/DatabaseController.ts
@@ -1,6 +1,7 @@
 import { Pool } from "pg";
 import { Kysely, PostgresDialect } from "kysely";
 import { Database } from "./models/Database";
+import { ConversationResponse } from "./models/ConversationResponse";
 
 const dialect = new PostgresDialect({
   pool: new Pool({
@@ -21,3 +22,52 @@ const dialect = new PostgresDialect({
 export const db = new Kysely<Database>({
   dialect,
 });
+
+export const getFullConversation = async (
+  conv_id: string
+): Promise<ConversationResponse> => {
+  const conversationData = await db
+    .selectFrom("conversations")
+    .leftJoin("contacts", "contacts.id", "conversations.contact_id")
+    .where("conversations.id", "=", conv_id)
+    .select([
+      "conversations.id",
+      "conversations.contact_id",
+      "contacts.name",
+      "contacts.email",
+      "contacts.phone",
+      "conversations.created_at",
+      "conversations.last_message_at",
+      "conversations.status",
+      "conversations.summary",
+      "conversations.sentiment",
+    ])
+    .executeTakeFirst();
+
+  if (conversationData) {
+    const messageData = await db
+      .selectFrom("messages")
+      .where("conversation_id", "=", conv_id)
+      .selectAll()
+      .execute();
+    const result: ConversationResponse = {
+      id: conversationData.id,
+      contact: {
+        id: conversationData.contact_id,
+        name: conversationData.name,
+        email: conversationData.email,
+        phone: conversationData.phone,
+        conversations: undefined,
+      },
+      messages: messageData,
+      created_at: conversationData.created_at,
+      last_message_at: conversationData.last_message_at,
+      status: conversationData.status,
+      summary: conversationData.summary,
+      sentiment: conversationData.sentiment,
+    };
+    return result;
+  } else {
+    throw "Can't find coversation";
+  }
+};
