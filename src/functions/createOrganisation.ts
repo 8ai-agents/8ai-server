18ab20ab5c4f088ca7b0e6f115f6fc572ef1b99e
James Bayly <james@bayly.xyz>
2024-05-16 17:29:19 +0800

    Revert "Improve build"
    This reverts commit 4cf3935fd6c88faa5fe61575a296520fa159b29a.

--- src/functions/createOrganisation.ts
+++ src/functions/createOrganisation.ts
@@ -9,11 +9,10 @@ import { NewOrganisation } from "../models/Database";
 import { OrganisationRequest } from "../models/OrganisationRequest";
 import { db, getOrganisation } from "../DatabaseController";
 import { checkUserIsAdmin, createID } from "../Utils";
-import OpenAI from "openai";
 
 export async function createOrganisation(
   request: HttpRequest,
-  context: InvocationContext
+  context: InvocationContext,
 ): Promise<HttpResponseInit> {
   try {
     const { email } = await authenticateRequest(request);
@@ -25,54 +24,7 @@ export async function createOrganisation(
   try {
     const organisationRequest = (await request.json()) as OrganisationRequest;
 
-    if (!organisationRequest.assistant_id) {
-      // Create a new assistant using OpenAI's JS SDK
-      try {
-        const openai = new OpenAI({
-          apiKey: process.env.OPEN_API_KEY,
-        });
-        const myAssistant = await openai.beta.assistants.create({
-          name: `8ai-${organisationRequest.name
-            .trim()
-            .split(" ")
-            .join("-")
-            .toLowerCase()}`,
-          instructions: `You are a customer support agent for ${organisationRequest.name}. Please answer concisely and nicely to potential customers, if you don't know the answer or the question is sensitive, please ask them to provide a phone number for a call back by an expert within 2 business days.`,
-          model: "gpt-4o",
-          tools: [
-            {
-              type: "function",
-              function: {
-                name: "save_contact_details",
-                description: "Save contact details of user to database",
-                parameters: {
-                  type: "object",
-                  properties: {
-                    name: {
-                      type: "string",
-                      description: "The user's name",
-                    },
-                    email: {
-                      type: "string",
-                      description: "The user's email",
-                    },
-                    phone: {
-                      type: "string",
-                      description: "The user's phone number",
-                    },
-                  },
-                  required: [],
-                },
-              },
-            },
-          ],
-        });
-        organisationRequest.assistant_id = myAssistant.id;
-      } catch (e) {
-        context.error(`Failed to create assistant in OpenAI: ${e.message}`);
-      }
-    }
-
+    context.log(`Creating organisation ${organisationRequest.name}`);
     const organisationToSave: NewOrganisation = {
       ...organisationRequest,
       id: createID("org"),
