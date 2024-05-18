33936644f1ea0219571b353e502c3a48d689afab
James Bayly <james@bayly.xyz>
2024-05-18 11:10:50 +0800

    Correctly process Slack messages

---
+++ src/functions/messageProcessor.ts
@@ -0,0 +1,77 @@
+import { app, EventGridEvent, InvocationContext } from "@azure/functions";
+import OpenAI from "openai";
+import { MessageResponse } from "../models/MessageResponse";
+import { processOpenAIMessage } from "../openAIHandler";
+
+export async function messageProcessor(
+  event: EventGridEvent,
+  context: InvocationContext
+): Promise<void> {
+  if (event.eventType === "Message.Slack") {
+    context.log("Incomming Slack Message: ", event);
+    await processSlackMessage(event, context);
+  } else {
+    context.log("Don't know how to process this event: ", event);
+  }
+}
+
+const processSlackMessage = async (
+  event: EventGridEvent,
+  context: InvocationContext
+) => {
+  /// Process
+  const openai = new OpenAI({
+    apiKey: process.env.OPEN_API_KEY,
+  });
+  const thread = await openai.beta.threads.createAndRunPoll(
+    {
+      assistant_id: event.data.assistant_id.toString(),
+      instructions: "",
+      thread: {
+        messages: [
+          {
+            role: "user",
+            content: event.data.message.toString(),
+          },
+        ],
+      },
+    },
+    { pollIntervalMs: 300 }
+  );
+  const messageResponse: MessageResponse[] = [];
+
+  if (thread.status === "completed") {
+    const messages = await openai.beta.threads.messages.list(thread.thread_id);
+    for (const message of messages.data.slice(
+      0,
+      messages.data.findIndex((m) => m.role === "user")
+    )) {
+      // Gets all messages from the assistant since last user message
+      if (message.content[0].type === "text") {
+        messageResponse.push(processOpenAIMessage(message, ""));
+      }
+    }
+  } else {
+    context.error(thread.status);
+    throw new Error("OpenAI request failed");
+  }
+
+  await fetch(event.data.response_url.toString(), {
+    method: "POST",
+    body: JSON.stringify({
+      response_type: "in_channel",
+      text: messageResponse.map((r) => r.message).join("\n"),
+    }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  })
+    .then((response) => context.log("Processed Slack Message"))
+    .catch((error) => {
+      context.log(error);
+    });
+};
+
+app.eventGrid("messageProcessor", {
+  handler: messageProcessor,
+});
--- src/functions/sendMessageSlack.ts
+++ src/functions/sendMessageSlack.ts
@@ -4,86 +4,106 @@ import {
   HttpResponseInit,
   InvocationContext,
 } from "@azure/functions";
-import OpenAI from "openai";
 import type { FormDataEntryValue } from "undici";
-import { handleMessageForOpenAI, processOpenAIMessage } from "../openAIHandler";
-import { MessageCreatorType, NewMessage } from "../models/Database";
-import { MessageRequest } from "../models/MessageRequest";
-import { MessageResponse } from "../models/MessageResponse";
-import { AzureKeyCredential, EventGridPublisherClient } from "@azure/eventgrid";
+import {
+  AzureKeyCredential,
+  EventGridPublisherClient,
+  SendEventGridEventInput,
+} from "@azure/eventgrid";
+import { db } from "../DatabaseController";
 
 export async function sendMessageSlack(
   request: HttpRequest,
   context: InvocationContext
 ): Promise<HttpResponseInit> {
-  /*
-  const org_id = request.params.org_id as string;
-  if (!org_id) {
-    return {
-      status: 400,
-      jsonBody: {
-        error: "Must supply a valid conversation ID",
-      },
-    };
-  }
-  */
-
-  let formData = await request.formData();
-  let data: FormDataEntryValue | null = formData.get("text");
-  if (data && typeof data == "string") {
-    context.log(`Processing message from Slack ${data}`);
-    const topicEndpoint =
-      "https://&lt;your-event-grid-topic&gt;.&lt;region&gt;-1.eventgrid.azure.net/api/events";
-    const topicKey = "8ai-messaging-topic";
-
-    const client = new EventGridPublisherClient(
-      topicEndpoint,
-      "EventGrid",
-      new AzureKeyCredential(topicKey)
-    );
-
-    const events = [
-      {
-        eventType: "MyEvent.Type",
-        subject: "MyEvent.Subject",
-        dataVersion: "1.0",
-        data: {
-          message: "Hello, Event Grid!",
-        },
-      },
-    ];
-
-    try {
-      await client.send(events);
+  try {
+    const org_id = request.params.org_id as string;
+    if (!org_id) {
       return {
-        status: 200,
+        status: 400,
         jsonBody: {
-          response_type: "in_channel",
-          text: "Assistant is thinking...",
-        },
-      };
-    } catch (error) {
-      return {
-        status: 500,
-        jsonBody: {
-          response_type: "in_channel",
-          text: "Assistant is thinking...",
+          error: "Must supply a valid organisation ID",
         },
       };
     }
-  }
 
-  return {
-    status: 500,
-    jsonBody: {
-      error: "Failed to send message",
-    },
-  };
+    let formData = await request.formData();
+    let data: FormDataEntryValue | null = formData.get("text");
+    let response_url: FormDataEntryValue | null = formData.get("response_url");
+    if (
+      data &&
+      typeof data == "string" &&
+      response_url &&
+      typeof response_url == "string"
+    ) {
+      context.log(`Processing message from Slack ${data}`);
+      const { assistant_id } = await db
+        .selectFrom("organisations")
+        .select(["assistant_id"])
+        .where("id", "=", org_id)
+        .executeTakeFirst();
+
+      const topicEndpoint =
+        "https://8ai-messaging-topic.australiaeast-1.eventgrid.azure.net/api/events";
+      const topicKey = process.env.MESSAGE_PROCESSOR_TOPIC_KEY;
+
+      const client = new EventGridPublisherClient(
+        topicEndpoint,
+        "EventGrid",
+        new AzureKeyCredential(topicKey)
+      );
+
+      const events: SendEventGridEventInput<{
+        assistant_id: string;
+        message: string;
+        response_url: string;
+      }>[] = [
+        {
+          eventType: "Message.Slack",
+          subject: `message/slack/${org_id}`,
+          dataVersion: "1.0",
+          data: {
+            assistant_id,
+            message: data,
+            response_url,
+          },
+        },
+      ];
+
+      try {
+        await client.send(events);
+        return {
+          status: 200,
+          jsonBody: {
+            response_type: "in_channel",
+            text: "Assistant is thinking...",
+          },
+        };
+      } catch (error) {
+        return {
+          status: 500,
+          jsonBody: {
+            response_type: "in_channel",
+            text: "Assistant is thinking...",
+          },
+        };
+      }
+    }
+    throw new Error("Can't parse Slack message");
+  } catch (e) {
+    context.error(e);
+    return {
+      status: 500,
+      jsonBody: {
+        error: "Failed to send message",
+      },
+    };
+  }
 }
 
 app.http("sendMessageSlack", {
   methods: ["POST"],
   authLevel: "anonymous",
-  route: "/chat/slack",
+  route: "/chat/slack/{org_id}",
   handler: sendMessageSlack,
 });
