39f78f0eb44a501583087896bf74fa71db7b5d14
James Bayly <james@bayly.xyz>
2024-05-18 10:24:21 +0800

    Event grid processor

--- src/functions/sendMessageSlack.ts
+++ src/functions/sendMessageSlack.ts
@@ -10,6 +10,7 @@ import { handleMessageForOpenAI, processOpenAIMessage } from "../openAIHandler";
 import { MessageCreatorType, NewMessage } from "../models/Database";
 import { MessageRequest } from "../models/MessageRequest";
 import { MessageResponse } from "../models/MessageResponse";
+import { AzureKeyCredential, EventGridPublisherClient } from "@azure/eventgrid";
 
 export async function sendMessageSlack(
   request: HttpRequest,
@@ -31,51 +32,45 @@ export async function sendMessageSlack(
   let data: FormDataEntryValue | null = formData.get("text");
   if (data && typeof data == "string") {
     context.log(`Processing message from Slack ${data}`);
-    const openai = new OpenAI({
-      apiKey: process.env.OPEN_API_KEY,
-    });
-    const assistant_id = "asst_rkDgpBkruW7HZqC0wwesebY2";
-    const thread = await openai.beta.threads.createAndRunPoll(
+    const topicEndpoint =
+      "https://&lt;your-event-grid-topic&gt;.&lt;region&gt;-1.eventgrid.azure.net/api/events";
+    const topicKey = "8ai-messaging-topic";
+
+    const client = new EventGridPublisherClient(
+      topicEndpoint,
+      "EventGrid",
+      new AzureKeyCredential(topicKey)
+    );
+
+    const events = [
       {
-        assistant_id,
-        instructions: "",
-        thread: {
-          messages: [
-            {
-              role: "user",
-              content: data,
-            },
-          ],
+        eventType: "MyEvent.Type",
+        subject: "MyEvent.Subject",
+        dataVersion: "1.0",
+        data: {
+          message: "Hello, Event Grid!",
         },
       },
-      { pollIntervalMs: 300 }
-    );
-    const messageResponse: MessageResponse[] = [];
+    ];
 
-    if (thread.status === "completed") {
-      const messages = await openai.beta.threads.messages.list(
-        thread.thread_id
-      );
-      for (const message of messages.data.slice(
-        0,
-        messages.data.findIndex((m) => m.role === "user")
-      )) {
-        // Gets all messages from the assistant since last user message
-        if (message.content[0].type === "text") {
-          messageResponse.push(processOpenAIMessage(message, ""));
-        }
-      }
-    } else {
-      context.error(thread.status);
-      throw new Error("OpenAI request failed");
+    try {
+      await client.send(events);
+      return {
+        status: 200,
+        jsonBody: {
+          response_type: "in_channel",
+          text: "Assistant is thinking...",
+        },
+      };
+    } catch (error) {
+      return {
+        status: 500,
+        jsonBody: {
+          response_type: "in_channel",
+          text: "Assistant is thinking...",
+        },
+      };
     }
-    return {
-      status: 200,
-      jsonBody: {
-        response_type: "in_channel",
-        text: messageResponse.map((r) => r.message).join("\n"),
-      },
-    };
   }
 
   return {
